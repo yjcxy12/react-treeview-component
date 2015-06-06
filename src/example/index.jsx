d6ccdc7d3b1465f7cf76a6b14e855d0e28001fb5
yjcxy12 <yjcxy12@gmail.com>
2015-06-06 22:25:38 +0100

    some documentation

--- src/example/index.jsx
+++ src/example/index.jsx
@@ -1,82 +1,82 @@
 var dataSource = [{
-  'id': '1',
-  'text': 'myNode',
-  'icon': 'glyphicon glyphicon-folder-open',
-  'opened': false,
-  'disabled': false,
-  'selected': false,
-  'children': [{
-      'id': '3',
-      'text': 'myNode3',
-      'icon': 'glyphicon glyphicon-folder-open',
-      'opened': false,
-      'disabled': false,
-      'selected': false,
-      'children': [
+  "id": "1",
+  "text": "myNode",
+  "icon": "glyphicon glyphicon-folder-open",
+  "opened": false,
+  "selected": false,
+  "children": [{
+      "id": "3",
+      "text": "myNode3",
+      "icon": "glyphicon glyphicon-folder-open",
+      "opened": false,
+      "selected": false,
+      "children": [
         {
-          'id': '5',
-          'text': 'myNode5',
-          'icon': 'glyphicon glyphicon-leaf',
-          'opened': false,
-          'disabled': false,
-          'selected': false,
-          'children': []
+          "id": "5",
+          "text": "myNode5",
+          "icon": "glyphicon glyphicon-leaf",
+          "opened": false,
+          "selected": false,
+          "children": []
         }
       ]
     },
     {
-      'id': '4',
-      'text': 'myNode4',
-      'icon': 'glyphicon glyphicon-leaf',
-      'opened': false,
-      'disabled': false,
-      'selected': false,
-      'children': []
+      "id": "4",
+      "text": "myNode4",
+      "icon": "glyphicon glyphicon-leaf",
+      "opened": false,
+      "selected": false,
+      "children": []
     }
   ]
 },
 {
-  'id': '2',
-  'text': 'myNode2',
-  'icon': 'glyphicon glyphicon-folder-open',
-  'opened': false,
-  'disabled': false,
-  'selected': false,
-  'children': []
+  "id": "2",
+  "text": "myNode2",
+  "icon": "glyphicon glyphicon-folder-open",
+  "opened": false,
+  "selected": false,
+  "children": []
 }];
 
 var Sample = React.createClass({
-  getInitialState: function () {
-      return {
-          dataSource: this.props.dataSource  
-      };
-  },
-  handleTreenodeClick: function (node_id) {
+
+  handleTreenodeClick: function (node_id, event) {
     var data = {
       id: '#',
       children: this.props.dataSource
     };
-    iter(data);
-    function iter(node) {
-      for (var i = 0, arrlen = node.children.length; i < arrlen; i++) {
-        var child = node.children[i];
-        iter(child);
-      }
+
+    iter(data, event.ctrlKey);
+    
+    this.forceUpdate();
+
+    function iter(node, ctrl) {
+      node.children.map(function (child) {
+        iter(child, ctrl);
+      });
       if (node_id === node.id) {
-        node.text += ' click';
+        node.selected = !node.selected;
+      }
+      else {
+        if (!ctrl) {
+          node.selected = false;
+        }
       }
     }
-
-    this.setState(this.state.dataSource);
   },
 
   render: function() {
     var that = this;
     return (
-      <Treeview dataSource={this.props.dataSource} 
-        onTreenodeClick={this.handleTreenodeClick}
-        ref='treeview'>
-      </Treeview>
+      <div>
+        <button className="btn btn-default">Change Name</button>
+        <Treeview dataSource={this.props.dataSource}
+          onTreenodeClick={this.handleTreenodeClick}
+          ref='treeview'>
+        </Treeview>
+      </div>
     );
   }
 });
